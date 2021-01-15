f59a7721c7aba7f34febfaf28bae0ff0b40174bb
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2021-01-15 01:18:20 +0100

    npm build proba

---
+++ src/coincap/websocket.js
@@ -0,0 +1,7 @@
+const WebSocket = require('ws');
+
+const pricesWs = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin')
+
+pricesWs.onmessage = function (msg) {
+    console.log(msg.data)
+}
